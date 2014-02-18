c25ffef6c8bedee445a3a110c4cc457a08a1adbe
Emily Wilder <emilywilder.earth@gmail.com>
2014-02-18 10:57:27 -0500

    move all logic inside class
    class now takes minimun and maxiumum disks we're interested in
    price distribution is hard coded into class
    
    add ability to output in csv friendly format for use in spreadsheets

--- raidzstats.py
+++ raidzstats.py
@@ -1,6 +1,13 @@
 class RaidzStats:
-    def __init__(self, raidzlevel=1):
+    def __init__(self, raidzlevel=1, mindisks=3, maxdisks=9):
         self.raidzlevel = raidzlevel
+        self.prices = {
+            2: 100,
+            3: 130,
+            4: 180
+        }
+        self.mindisks = mindisks
+        self.maxdisks = maxdisks
 
     def _calcstorage(self, disks, size):
         return disks * size - (self.raidzlevel * size)
@@ -11,23 +18,22 @@ class RaidzStats:
     def _calccostpertb(self, disks, size, cost):
         return self._calccost(disks, cost) / self._calcstorage(disks, size)
 
-    def getstats(self, disks, size, cost):
-        print("{2} * {3} TB disks: {0} TB, ${1}/TB, ${4}".format(
-                  self._calcstorage(disks, size),
-                  self._calccostpertb(disks, size, cost),
-                  disks,
-                  size,
-                  self._calccost(disks, cost)))
+    def printstats(self, csv=False):
+        formatstring = "{2} * {3} TB disks: {0} TB, ${1}/TB, ${4}"
+        if csv:
+            print("Configuration (RAIDZ{0}),Redundant Storage (in TB),$USD/TB,Total $USD".format(self.raidzlevel))
+            formatstring = "{2}*{3} TB disks,{0},${1},${4}"
+
+        for disks in xrange(self.mindisks, self.maxdisks+1):
+            for size, cost in self.prices.items(): 
+                print(formatstring.format(
+                    self._calcstorage(disks, size),
+                    self._calccostpertb(disks, size, cost),
+                    disks,
+                    size,
+                    self._calccost(disks, cost)))
 
 if __name__ == "__main__":
-    prices = {
-        2: 100,
-        3: 130,
-        4: 180
-    }
-
-    rs = RaidzStats(raidzlevel=2)
-    for n in xrange(3, 10):
-        for s, p in prices.items(): 
-            rs.getstats(n, s, p)
+    rs = RaidzStats(raidzlevel=1, maxdisks=6)
+    rs.printstats(csv=True)
 
